a0c74377169041df04f75bf1ab420ff6e88a2d96
Jaime Ansorena <jaimeansorenac@gmail.com>
2022-10-21 18:28:33 -0300

    consulta precio pedido

--- consultas.sql
+++ consultas.sql
@@ -9,7 +9,7 @@ INSERT INTO ingrediente(id_ingrediente,nombre,stock,u_m,fecha_exp,valor_unitario
 (8,'lechuga',28,'kg','2022-10-25',1000);
 
 INSERT INTO producto(id_producto,nombre,porcentaje_ganancia,tmp_preparacion) VALUES
-(1234,'Hamburguesa',20,20);
+(1234,'Hamburguesa1',20,20);
 
 INSERT INTO compone(id_producto,id_ingrediente,cantidad_ingrediente) VALUES
 (1234,1,1),
@@ -20,6 +20,32 @@ INSERT INTO compone(id_producto,id_ingrediente,cantidad_ingrediente) VALUES
 (1234,6,2),
 (1234,7,0.2),
 (1234,8,0.2);
+
+INSERT INTO producto(id_producto,nombre,porcentaje_ganancia,tmp_preparacion) VALUES
+(1235,'Hamburguesa2',20,20);
+
+INSERT INTO compone(id_producto,id_ingrediente,cantidad_ingrediente) VALUES
+(1235,1,1),
+(1235,2,4),
+(1235,3,3),
+(1235,4,4),
+(1235,5,1),
+(1235,6,2),
+(1235,7,0.4),
+(1235,8,0.4);
+
+INSERT INTO persona(RUT,nombre,apellido) VALUES
+('20.123.456-7','Pedro','Perez');
+
+INSERT INTO cliente(RUT) VALUES
+('20.123.456-7');
+
+INSERT INTO pedido (id_pedido,RUT) VALUES
+(1,'20.123.456-7');
+
+INSERT INTO tiene VALUES
+(1,1234,3),
+(1,1235,2);
 
 /* Precio Hamburguesa */
 
@@ -36,5 +62,20 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION precio_pedido(numero_pedido INT)
+RETURNS REAL AS $$
+DECLARE precio_pedido REAL;
+BEGIN
+	precio_pedido := (
+		SELECT sum(t.cantidad_producto*precio(pr.nombre))
+		FROM pedido as pe, tiene as t, producto as pr
+		WHERE pe.id_pedido = numero_pedido AND pe.id_pedido = t.id_pedido AND t.id_producto = pr.id_producto
+		);
+	RETURN precio_pedido;
+END
+$$ LANGUAGE plpgsql;
 
 SELECT precio('Hamburguesa');
+SELECT precio_pedido(1);
+
+

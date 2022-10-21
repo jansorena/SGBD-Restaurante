5b2c8719240d053b3e2612c11e84bfb3165e8979
Jaime Ansorena <jaimeansorenac@gmail.com>
2022-10-21 02:45:18 -0300

    agrega constraints

--- proyecto.sql
+++ proyecto.sql
@@ -1,37 +1,37 @@
 CREATE SCHEMA proyecto;
 
 CREATE TABLE persona(
-RUT    VARCHAR(100) PRIMARY KEY,
-nombre VARCHAR(100),
-apellido VARCHAR(100));
+RUT VARCHAR(100) PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+apellido VARCHAR(100) NOT NULL);
 
 CREATE TABLE cliente(
-RUT VARCHAR(100) PRIMARY KEY REFERENCES persona(RUT));
+RUT VARCHAR(100) PRIMARY KEY REFERENCES persona(RUT) ON DELETE RESTRICT ON UPDATE CASCADE);
 
 CREATE TABLE trabajador(
-RUT VARCHAR(100) PRIMARY KEY REFERENCES persona(RUT));
+RUT VARCHAR(100) PRIMARY KEY REFERENCES persona(RUT) ON DELETE RESTRICT ON UPDATE CASCADE);
 
 CREATE TABLE mesa(
 num_mesa INT PRIMARY KEY,
-estado_mesa VARCHAR(100));
+estado_mesa VARCHAR(100) CHECK(estado_mesa IN ('libre','ocupada')));
 
 CREATE TABLE pedido(
 id_pedido INT PRIMARY KEY,
 valor_pedido INT,
-estado_pedido VARCHAR(100),
-fecha_pedido DATE,
-RUT VARCHAR(100) REFERENCES cliente(RUT));
+estado_pedido VARCHAR(100) DEFAULT 'no entregado' CHECK(estado_pedido IN ('entregado','no entregado')),
+fecha_pedido DATE DEFAULT CURRENT_DATE,
+RUT VARCHAR(100) REFERENCES cliente(RUT) ON DELETE RESTRICT ON UPDATE CASCADE);
 
 CREATE TABLE en_local(
-id_pedido INT PRIMARY KEY REFERENCES pedido(id_pedido));
+id_pedido INT PRIMARY KEY REFERENCES pedido(id_pedido) ON DELETE RESTRICT ON UPDATE CASCADE));
 
 CREATE TABLE fuera_local(
-id_pedido INT PRIMARY KEY REFERENCES pedido(id_pedido));
+id_pedido INT PRIMARY KEY REFERENCES pedido(id_pedido) ON DELETE RESTRICT ON UPDATE CASCADE));
 
 CREATE TABLE producto(
 id_producto INT PRIMARY KEY,
 valor_producto INT,
-porcentaje_ganancia INT,
+porcentaje_ganancia INT DEFAULT 20,
 tmp_preparacion INT);
 
 CREATE TABLE boleta(
@@ -39,8 +39,8 @@ id_boleta INT PRIMARY KEY,
 total INT,
 valor_neto INT,
 iva INT,
-id_pedido INT REFERENCES pedido(id_pedido),
-fecha_venta DATE);
+id_pedido INT REFERENCES pedido(id_pedido) ON DELETE RESTRICT ON UPDATE CASCADE),
+fecha_venta DATE DEFAULT CURRENT_DATE);
 
 CREATE TABLE ingrediente(
 id_ingrediente INT PRIMARY KEY,
@@ -51,18 +51,18 @@ valor_unitario INT);
 
 CREATE TABLE egreso(
 id_egreso INT PRIMARY KEY,
-fecha_egreso DATE,
+fecha_egreso DATE DEFAULT CURRENT_DATE,
 descripcion VARCHAR(100),
 total INT);
 
 CREATE TABLE compra(
-id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso));
+id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso) ON DELETE RESTRICT ON UPDATE CASCADE));
 
 CREATE TABLE pago_trabajador(
-id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso));
+id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso) ON DELETE RESTRICT ON UPDATE CASCADE));
 
 CREATE TABLE otro_egreso(
-id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso));
+id_egreso INT PRIMARY KEY REFERENCES egreso(id_egreso) ON DELETE RESTRICT ON UPDATE CASCADE));
 
 CREATE TABLE proveedor(
 id_proveedor INT PRIMARY KEY,
@@ -71,33 +71,33 @@ empresa VARCHAR(100),
 num_contacto VARCHAR(100));
 
 CREATE TABLE ocupa(
-num_mesa INT REFERENCES mesa(num_mesa),
-id_pedido INT REFERENCES pedido(id_pedido),
-RUT INT REFERENCES cliente(RUT)
+num_mesa INT REFERENCES mesa(num_mesa) ON DELETE RESTRICT ON UPDATE CASCADE),
+id_pedido INT REFERENCES pedido(id_pedido) ON DELETE RESTRICT ON UPDATE CASCADE),
+RUT VARCHAR(100) REFERENCES cliente(RUT) ON DELETE RESTRICT ON UPDATE CASCADE),
 PRIMARY KEY(num_mesa,id_pedido,RUT));
 
 CREATE TABLE tiene(
-id_pedido INT REFERENCES pedido(id_pedido),
-id_producto INT REFERENCES producto(id_producto),
+id_pedido INT REFERENCES pedido(id_pedido) ON DELETE RESTRICT ON UPDATE CASCADE),
+id_producto INT REFERENCES producto(id_producto) ON DELETE RESTRICT ON UPDATE CASCADE),
 cantidad_producto INT,
 PRIMARY KEY (id_pedido,id_producto));
 
 CREATE TABLE compone(
-id_producto INT REFERENCES producto(id_producto),
-id_ingrediente INT REFERENCES ingrediente(id_ingrediente),
+id_producto INT REFERENCES producto(id_producto) ON DELETE RESTRICT ON UPDATE CASCADE),
+id_ingrediente INT REFERENCES ingrediente(id_ingrediente) ON DELETE RESTRICT ON UPDATE CASCADE),
 cantidad_ingrediente INT,
 PRIMARY KEY (id_producto,id_ingrediente));
 
 CREATE TABLE actualiza(
-id_ingrediente INT REFERENCES ingrediente(id_ingrediente),
-id_egreso INT REFERENCES compra(id_egreso),
-fecha_actualiza DATE,
+id_ingrediente INT REFERENCES ingrediente(id_ingrediente) ON DELETE RESTRICT ON UPDATE CASCADE),
+id_egreso INT REFERENCES compra(id_egreso) ON DELETE RESTRICT ON UPDATE CASCADE),
+fecha_actualiza DATE DEFAULT CURRENT_DATE,
 cantidad_actualiza INT,
 PRIMARY KEY(id_ingrediente,id_egreso));
 
 CREATE TABLE transaccion(
-id_proveedor INT REFERENCES proveedor(id_proveedor),
-id_egreso INT REFERENCES compra(id_egreso),
+id_proveedor INT REFERENCES proveedor(id_proveedor) ON DELETE RESTRICT ON UPDATE CASCADE),
+id_egreso INT REFERENCES compra(id_egreso) ON DELETE RESTRICT ON UPDATE CASCADE),
 PRIMARY KEY (id_proveedor,id_egreso));
 
 
